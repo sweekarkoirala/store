29b4a9acdd918454b0ed2c7a89f39d19953d3128
Sweekar <sweekar1001@gmail.com>
2024-12-09 18:05:16 +0530

    update mongoose-connection

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
-const password= "Zffw2uw6EAdFJU18";
 
 async function connectDB() {
   try {
-    const connection = await mongoose.connect(`mongodb+srv://22054440:${password}@cluster0.z88or.mongodb.net/Chess`);
+    const connection = await mongoose.connect(process.env.CONNECTION_STRING);
     console.log("Database connected successfully");
     return connection;
   } catch (error) {
