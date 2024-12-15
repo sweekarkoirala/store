4891e9ae5da415d9df0b2ce18f73230d5e813c3d
Sweekar <sweekar1001@gmail.com>
2024-12-15 22:51:22 +0530

    modify onwener router with create route, add try and catch in function

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
+const config = require("config");
 const dbgr = require("debug")("development:mongoose");
 
 mongoose
-.connect("mongodb://127.0.0.1:27017/scatch")
+.connect(`${config.get("MONGODB_URI")}/scatch`)
 .then(function(){
   dbgr("connected");
+  console.log("hi");
 })
 .catch(function(err){
   console.log(err);
--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const ownerModel = require("../models/owner-model");
 
-router.get("/", function ( req, res) {
+if (process.env.NODE_ENV === "development") {
+
+    router.post("/create", async function (req, res) {
+        try {
+            let owners = await ownerModel.find();
+            if (owners.length > 0) {
+                return res
+                    .status(503)
+                    .send("You don't have permission to create a new owner.");
+            }
+
+            let { fullname, email, password } = req.body;
+
+            let createdOwner = await ownerModel.create({
+                fullname,
+                email,
+                password,
+            });
+            res.status(201).send(createdOwner);
+        }
+        catch(err){
+            console.log(err.message);
+
+        }
+});
+}
+
+
+router.get("/", function (req, res) {
     res.send("hey");
 });
+
+
 
 
 module.exports = router;
